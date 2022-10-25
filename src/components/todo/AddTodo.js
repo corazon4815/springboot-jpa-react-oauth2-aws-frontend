286e0d7277a20b8da89325f3a58e0e78f518777c
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-10-25 10:50:16 +0900

    feat : 투두 추가 기능 개발

--- src/components/todo/AddTodo.js
+++ src/components/todo/AddTodo.js
@@ -7,15 +7,44 @@ const AddTodo = (props) => {
             title: ""
         });
 
+        const addItem = props.addItem;
+
+        // onButtonClick 함수 작성
+        const onButtonClick = () => {
+            addItem(item); // addItem 함수 사용
+            setItem({title: ""});
+        };
+
+        // onInputChange 함수 작성
+        const onInputChange = (e) => {
+            setItem({title: e.target.value});
+            console.log(item);
+        };
+
+        // enterKeyEventHandler 함수
+        const enterKeyEventHandler = (e) => {
+            if (e.key === 'Enter') {
+                onButtonClick();
+            }
+        };
+
         return (
             <React.Fragment>
                 <Grid container style={{marginTop: 20}}>
                     <Grid xs={11} md={11} item style={{paddingRight: 16}}>
-                        <TextField placeholder={"할일을 입력해주세요."} fullWidth/>
+                        <TextField placeholder={"할일을 입력해주세요."}
+                                   fullWidth
+                                   onChange={onInputChange}
+                                   onKeyPress={enterKeyEventHandler}
+                                   value={item.title}
+                        />
                     </Grid>
                     <Grid xs={1} md={1} item>
-                        <Button fullWidth style={{height: '100%'}} color={"secondary"}
-                                variant={"outlined"}>+</Button>
+                        <Button fullWidth
+                                style={{height: '100%'}}
+                                color={"secondary"}
+                                variant={"outlined"}
+                                onClick={onButtonClick}>+</Button>
                     </Grid>
                 </Grid>
             </React.Fragment>
