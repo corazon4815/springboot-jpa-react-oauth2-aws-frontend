17157763dbcd20c6503daaa6a0a69bf3fe12e79b
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-10-25 10:56:02 +0900

    feat : 백엔드 api 연동

--- src/App.js
+++ src/App.js
@@ -1,43 +1,52 @@
 import Todo from './components/todo/Todo.js'
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Container, List, Paper} from "@mui/material";
 import AddTodo from "./components/todo/AddTodo";
 
 function App() {
-    const [todoList, setTodoList] = useState([
-        {
-            id: "0",
-            title: "산책가기",
-            done: true
-        },
-        {
-            id: "1",
-            title: "독서하기",
-            done: true
-        },
-        {
-            id: "2",
-            title: "영어 공부하기",
-            done: false
-        }
-    ]);
+    const [todoList, setTodoList] = useState([]);
+
+    useEffect(() => {
+        call("/todo", "GET", null)
+            .then((response) => setItems(response.data));
+    }, []);
+
+    const addItem = (item) => {
+        call("/todo", "POST", item)
+            .then((response) => setItems(response.data));
+    };
+
+    const editItem = (item) => {
+        call("/todo", "PUT", item)
+            .then((response) => setItems(response.data));
+    };
+
+    const deleteItem = (item) => {
+        call("/todo", "DELETE", item)
+            .then((response) => setItems(response.data));
+    };
+
+    let todoItems = todoList.length > 0 && (
+        <Paper style={{ margin: 16 }}>
+            <List>
+                {todoList.map((item) => (
+                    <Todo
+                        item={item}
+                        key={item.id}
+                        editItem={editItem}
+                        deleteItem={deleteItem}
+                    />
+                ))}
+            </List>
+        </Paper>
+    );
 
     return (
         <div className="App">
             <Container maxWidth={"md"}>
-                <AddTodo/>
-                <div className={"TodoList"}>
-                    <Paper style={{margin: 16}}>
-                        <List>
-                            {
-                                todoList.length > 0 && todoList.map((item) => (
-                                    <Todo key={item.id} item={item}/>
-                                ))
-                            }
-                        </List>
-                    </Paper>
-                </div>
+                <AddTodo addItem={addItem} />
+                <div className="TodoList">{todoItems}</div>
             </Container>
         </div>
     );
