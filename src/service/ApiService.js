01b007d11b5372ddbd0d3040e0a4e669e739dde1
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-10-26 10:45:58 +0900

    fix : 요청시 쿠키를 안가져오는 문제

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,12 +1,15 @@
-import { API_BASE_URL } from "../api-config";
+import {API_BASE_URL} from "../api-config";
 
 export function call(api, method, request) {
+    let headers = new Headers({
+        "Content-Type": "application/json",
+    });
+
     let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
+        credentials: 'include', //쿠키 전달을 위해
     };
     if (request) {
         // GET method
@@ -15,8 +18,8 @@ export function call(api, method, request) {
     return fetch(options.url, options).then((response) => {
         if (response.status === 200) {
             return response.json();
-        } else if(response.status === 403) {
-            window.location.href = "/login"; // redirect
+        } else if (response.status === 403) {
+            return response;
         } else {
             new Error(response);
         }
@@ -24,4 +27,28 @@ export function call(api, method, request) {
         console.log("http error");
         console.log(error);
     });
+}
+
+/*
+ * 로그인
+ */
+export function signIn(userDTO) {
+    return call("/auth/login", "POST", userDTO)
+        .then((response) => {
+            return response;
+        });
+}
+
+/*
+ * 회원가입
+ */
+export function signUp(userDTO) {
+    return call("/auth/signup", "POST", userDTO);
+}
+
+/*
+ * 로그아웃
+ */
+export function signOut() {
+
 }
