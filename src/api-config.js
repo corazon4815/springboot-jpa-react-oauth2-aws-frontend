8bb6e3afe19dfed2903bbdbd269bcf7658d77fc7
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-10-26 10:44:35 +0900

    refactor : 서버 port 변경

--- src/api-config.js
+++ src/api-config.js
@@ -4,7 +4,7 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
-    backendHost = "http://localhost:8080";
+    backendHost = "http://localhost:8111";
 }
 
 export const API_BASE_URL = `${backendHost}`;
