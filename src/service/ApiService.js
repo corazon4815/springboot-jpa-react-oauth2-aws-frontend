56e977aceaf31a82e5ede6e22da6ec1a7e621a0b
parksujin <mango4815@neighbor21.co.kr>
2022-10-31 16:52:57 +0900

    refactor : axois 인스턴스 추가

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -2,44 +2,32 @@ import axios from "axios";
 import {todo} from "../components/todo/TodoApi";
 import {user} from "../components/user/UserApi";
 
-axios.defaults.withCredentials = true;
-axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8';
-axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
-axios.defaults.timeout = 10000;
-
-axios.interceptors.request.use(function (config) {
-    console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-    return config;
-}, function (error) {
-    return Promise.reject(error);
+const _axios = axios.create({
+    withCredentials : true,
+    headers: {
+        "Content-Type": "application/json;charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+    },
+    timeout : 10000,
 });
 
-axios.interceptors.response.use(function (response) {
-    console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@111")
-    const {data, headers} = response;
-
-
-    if (data.code === 401) {
-    }
-    /* switch (data.code) {
-         case 400:
-             break;
-         case 401:
-             sessionStorage.removeItem("session");
-             sessionStorage.removeItem("token");
-             break;
-     }*/
-
-    return response;
-}, function (error) {
-
-    setTimeout(() => {
+_axios.interceptors.request.use(
+    (config) => {
+        return config;
+    },
+    (error) => {
         return Promise.reject(error);
-    });
+    }
+);
 
-    // 응답 에러 시에도 로딩 끄기
-    //return Promise.reject(error);
-});
+_axios.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default {
     todo,
